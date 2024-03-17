8109f1ea07ccfe3d4805806a4e0f5c50d7af922e
vaibhavchavhan45 <chavhanvaibhav708@gmail.com>
2024-03-18 00:38:38 +0530

    datatype summary

---
+++ 01_basics/06_datatypeSummary.js
@@ -0,0 +1,34 @@
+//Primitive datatypes
+//String, Number, BigInt, Boolean, Symbol, undefined, null 
+
+
+//Non-Primitive datatypes (reference)
+//Array, Function, object
+
+//Array
+let array = ["v", "a", "c"]
+console.log(typeof array);
+//typeof Array is object
+
+
+
+//function
+let func = function a() {
+    console.log("vc");
+}
+console.log(typeof func);
+//typeof function is function
+
+
+
+//object ==> object
+let obj = {
+     name :"vaibhav",
+     cityName : "Arni",
+     age : 25
+}
+console.log(typeof obj);
+//typeof object is object
+
+
+//Link of typeof Operator Results ==>  https://262.ecma-international.org/5.1/#sec-11.4.3
