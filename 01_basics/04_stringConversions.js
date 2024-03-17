68393486390f6aa45750797074c1d84ab5efba0b
vaibhavchavhan45 <chavhanvaibhav708@gmail.com>
2024-03-17 20:07:37 +0530

    String conversions

--- 01_basics/04_stringConversions.js
+++ 01_basics/04_stringConversions.js
@@ -18,7 +18,8 @@ console.log(1 + 1 + "2");  //first performs operations then concatinate string t
 //Type Coercion and Unary Operations 
 console.log(+true);     // ** 1 ** ==> convert boolean true to number i.e. 1 then performs operation
 //console.log(false+); // ** Error ** because unary operator needs value to its right
-console.log(+ "");    // ** 0 **   ==> value of empty string in JS is zero (0) 
+console.log(+ "");    // ** 0 **   ==> value of empty string in JS is zero (0)
+ 
 
 
 
