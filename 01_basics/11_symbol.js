8a3e21153aa83e6244f569596527a5e1df2e253a
vaibhavchavhan45 <chavhanvaibhav708@gmail.com>
2024-03-29 10:53:19 +0530

    Javascript

---
+++ 01_basics/11_symbol.js
@@ -0,0 +1,18 @@
+//Creating symbol
+const mySym = Symbol ("key")
+console.log(mySym);
+console.log(typeof mySym);
+
+const newSym = Symbol(5)
+console.log(typeof mySym);
+console.log(newSym);
+
+const sym = Symbol(true)
+console.log(typeof sym);
+console.log(sym);
+console.log(typeof sym);
+
+const myNewSym = Symbol ()
+console.log(myNewSym);
+
+
