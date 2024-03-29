1b468f1b9eea36b6219d107c587add0cc6ca28a6
vaibhavchavhan45 <chavhanvaibhav708@gmail.com>
2024-03-29 10:55:09 +0530

    objects 03

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,13 @@
+//Destructuring of objects
+const course = {
+    courseName : "BE in Extc",
+    courseDuration : "4yrs",
+    courseInstructor : "Rohit sir"
+}
+console.log(course.courseName);
+            //OR
+const {courseInstructor} = course     //Extraxt the value of courseInstructor from course object
+console.log(courseInstructor);
+            //OR
+const {courseDuration : time} = course //Extraxt the value of courseDuration from course object also change the key name courseDuration to time(destructuring of objects)
+console.log(time);
