42bc0e8503ef009d55b56c2ad248b398a872d53c
vaibhavchavhan45 <chavhanvaibhav708@gmail.com>
2024-03-30 15:51:07 +0530

    switch statement

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,31 @@
+const month = 3
+switch (month) {
+
+    case 1: console.log("January");    
+    break;
+    case 2: console.log("February");    
+    break;
+    case 3: console.log("March");    
+    break;
+    case 4: console.log("April");    
+    break;
+    case 1: console.log("May");    
+    break;
+
+    default: console.log("default case match");
+        break;
+}
+
+const week = "saturday"
+switch(week){
+    case "monday" : console.log("monday");
+    break;
+    case "wednesday" : console.log("wednesday");
+    break;
+    case "friday" : console.log("monday");
+    break;
+    case "saturday" : console.log("saturday");
+    break;
+    default : console.log("default case match");
+    break;
+}
