9776ab605f2f3b10dcdc4dba1d4346d3f4611114
vaibhavchavhan45 <chavhanvaibhav708@gmail.com>
2024-03-31 00:20:12 +0530

    while loop

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,12 @@
+let i = 1
+while (i<=100) {
+    console.log(`value of index is ${i}`);
+    i = i+4
+}
+
+myArr = ['vc', 'ac', 'dc', 'az', 'cz']
+let j=0
+while (j < myArr.length) {
+    console.log(myArr[j]);
+    j++
+}
