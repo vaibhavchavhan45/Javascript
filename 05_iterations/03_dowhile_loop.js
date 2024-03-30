5e3f45e504c434fe29259a87bc3de03567f4a9e5
vaibhavchavhan45 <chavhanvaibhav708@gmail.com>
2024-03-31 00:20:31 +0530

    do while loop

---
+++ 05_iterations/03_dowhile_loop.js
@@ -0,0 +1,21 @@
+let score = 1
+do {
+    console.log(`Our score is ${score}`);
+    score++
+} while (score <= 10);
+
+//Print atleast one time
+let i = 0
+do {
+    console.log("Vaibhav");
+    i++
+} while (i >= 10);
+
+
+//Print elements of array
+let arr = [1, 2, 3, 4, 5]
+let j = 0
+do {
+    console.log(arr[j]);
+    j++
+} while (j < arr.length);
