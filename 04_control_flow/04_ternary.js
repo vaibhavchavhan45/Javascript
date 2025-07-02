2f20bd97ceacbfe141fdf1edb8a992f854f2c923
vaibhavchavhan45 <chavhanvaibhav708@gmail.com>
2025-07-02 21:48:03 +0530

    ternary operator

--- 04_control_flow/04_ternary.js
+++ 04_control_flow/04_ternary.js
@@ -1,6 +1,10 @@
 //Nullish Coalescing operator (??) : null undefined
 
-/*(In JavaScript, the ?? operator is called the nullish coalescing operator. It returns the right-hand operand when the left-hand operand is null or undefined, otherwise, it returns the left-hand operand.)*/
+/*(In JavaScript, the ?? operator is called the nullish coalescing operator. It returns the right-hand operand when the left-hand operand is null or undefined, otherwise, it returns the left-hand operand.)
+
+In simple words : It returns the first value which is not null or undefined.
+*/
+
 
 
 let value;
